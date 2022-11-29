ce9d67d7c0b8364d91ab5793c316a338a1faf9a1
shafucheng <sha0329@outlook.com>
2022-11-29 16:41:25 +0800

    chore: use eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
+import { fileURLToPath } from 'node:url'
+
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslint({
+      fix: true,
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
