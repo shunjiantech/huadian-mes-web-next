d38632ba11f88a1151e5aa9e16ed7263fb445faa
shafucheng <sha0329@outlook.com>
2022-11-30 16:02:20 +0800

    chore: add vite config define

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
-import { fileURLToPath } from 'node:url'
+import { readFileSync } from 'node:fs'
+import path from 'node:path'
+import { fileURLToPath, URL } from 'node:url'
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import eslint from 'vite-plugin-eslint'
 
+const packageJson = JSON.parse(
+  readFileSync(path.resolve(__dirname, 'package.json'), 'utf-8'),
+)
+
 export default defineConfig({
+  define: {
+    __APP_NAME__: `"${packageJson.name}"`,
+    __APP_VERSION__: `"${packageJson.version}"`,
+  },
   plugins: [
     react(),
     eslint({
