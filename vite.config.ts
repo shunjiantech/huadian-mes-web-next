b433c231674631bd70c7d3eca467b3a521a4df5f
shafucheng <sha0329@outlook.com>
2022-11-30 18:29:29 +0800

    chore: auto import antd style

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 import unocss from 'unocss/vite'
 import { defineConfig } from 'vite'
 import eslint from 'vite-plugin-eslint'
+import { createStyleImportPlugin as styleImport } from 'vite-plugin-style-import'
 
 const packageJson = JSON.parse(
   readFileSync(path.resolve(__dirname, 'package.json'), 'utf-8'),
@@ -21,6 +22,15 @@ export default defineConfig({
     unocss(),
     eslint({
       fix: true,
+    }),
+    styleImport({
+      libs: [
+        {
+          libraryName: 'antd',
+          esModule: true,
+          resolveStyle: (name) => `antd/es/${name}/style/index`,
+        },
+      ],
     }),
   ],
   resolve: {
