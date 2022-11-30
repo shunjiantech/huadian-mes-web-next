8f5ffe32a09587802ccf7ba3198ef4b713b44e3b
shafucheng <sha0329@outlook.com>
2022-11-30 16:55:45 +0800

    chore: add less

--- vite.config.ts
+++ vite.config.ts
@@ -26,4 +26,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
 })
