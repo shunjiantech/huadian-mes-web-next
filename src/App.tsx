c89cee769fb8efd53c31dabfc40e5c0cdd82641c
shafucheng <sha0329@outlook.com>
2022-11-30 18:59:10 +0800

    feat: set antd locale=zhCN

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { Button } from 'antd'
+import 'dayjs/locale/zh-cn'
+
+import { Button, ConfigProvider } from 'antd'
+import zhCN from 'antd/es/locale/zh_CN'
 import { useState } from 'react'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <Button onClick={() => setCount((count) => count + 1)}>
-      count is {count}
-    </Button>
+    <ConfigProvider locale={zhCN}>
+      <Button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </Button>
+    </ConfigProvider>
   )
 }
 
