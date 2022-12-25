602479c74aeb69b6d54536fb8123f1d8009c1ed5
shafucheng <sha0329@outlook.com>
2022-12-25 19:06:36 +0800

    remove console.log

--- src/utils/useDataSource.ts
+++ src/utils/useDataSource.ts
@@ -1,5 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const useSyncDataSource = (dataSource: unknown) => (field: any) => {
-  console.log(dataSource)
   field.dataSource = dataSource
 }
