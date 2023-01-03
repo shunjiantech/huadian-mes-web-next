ed4b104cee64e63b552eaf9cff59cbe5d159dfd1
shafucheng <sha0329@outlook.com>
2023-01-03 19:50:25 +0800

    feat: set dayjs locale zh-cn

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import 'dayjs/locale/zh-cn'
 
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/es/locale/zh_CN'
+import dayjs from 'dayjs'
 import { BrowserRouter, useRoutes } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
 
 import routes from '@/routes'
+
+dayjs.locale('zh-cn')
 
 const Routes = () => {
   const element = useRoutes(routes)
